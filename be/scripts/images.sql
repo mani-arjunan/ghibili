ad49490ecc6e57881f81a2524ae1114454df2755
Manikandan Arjunan <31544468+mani-arjunan@users.noreply.github.com>
2025-03-31 21:48:36 +0530

    Update images.sql

--- be/scripts/images.sql
+++ be/scripts/images.sql
@@ -1,7 +1,7 @@
 CREATE TABLE public.images (
     id SERIAL PRIMARY KEY,
     user_id INT NOT NULL REFERENCES public.users(id) ON DELETE CASCADE,
-    image_urk VARCHAR(1000) NOT NULL,
+    image_url VARCHAR(1000) NOT NULL,
     status VARCHAR(50) NOT NULL
 );
 
